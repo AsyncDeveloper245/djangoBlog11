2cabf63e809714a3b367eb535ca606cc5347f680
AsyncDeveloper245 <AsyncDeveloper245@users.noreply.github.com>
2021-04-29 15:24:24 +0100

    add new files

--- blog/urls.py
+++ blog/urls.py
@@ -4,13 +4,13 @@ from .views import BlogListView,BlogDetailView,BlogCreateView,BlogUpdateView,Blo
 from django.contrib.auth.decorators import  login_required
 
 urlpatterns = [
-    path('post/',login_required(BlogListView.as_view()),name='home'),
+    path('',BlogListView.as_view(),name='home'),
     path('post/<int:id>/',login_required(BlogDetailView),name='post_detail'),
-    path('post/new',BlogCreateView.as_view(),name='post_new'),
-    path('post/<int:pk>/edit',BlogUpdateView.as_view(),name='post_edit'),
-    path('post/<int:pk>/delete',BlogDeleteView.as_view(),name='post_delete'),
+    path('post/new',login_required(BlogCreateView.as_view()),name='post_new'),
+    path('post/<int:pk>/edit',login_required(BlogUpdateView.as_view()),name='post_edit'),
+    path('post/<int:pk>/delete',login_required(BlogDeleteView.as_view()),name='post_delete'),
     path('register/',RegistrationFormView,name='register'),
     path('login/',LoginView,name='login'),
-    path('',HomeTemplateView.as_view(),name='landing'),
+    #path('',HomeTemplateView.as_view(),name='landing'),
     path('user/<int:id>/logout/',Logout,name='logout'),
 ]
