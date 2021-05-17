4870aeebef7b7654416a0db702f64f19e4bfded8
AsyncDeveloper245 <AsyncDeveloper245@users.noreply.github.com>
2021-05-18 00:21:07 +0100

    new structure

--- src/djangoFolder/settings.py
+++ src/djangoFolder/settings.py
@@ -22,10 +22,10 @@ import os
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=os.getenv('SECRET_KEY')
+SECRET_KEY=os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =os.getenv('DEBUG')
+DEBUG =os.getenv("DEBUG")
 
 ALLOWED_HOSTS = [ 'zuridjangoapp.herokuapp.com']
 
