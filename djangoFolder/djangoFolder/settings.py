a2387210a2d521b661aa0a02a337831a5d12774a
AsyncDeveloper245 <AsyncDeveloper245@users.noreply.github.com>
2021-05-19 02:06:06 +0100

    add new files

--- djangoFolder/djangoFolder/settings.py
+++ djangoFolder/djangoFolder/settings.py
@@ -22,11 +22,11 @@ import os
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=os.environ.get('SECRET_KEY')
-#SECRET_KEY='phzoz(zq=!&pca(m3x8qwt*(9xs@7qkv^h8d0*-y$buo3%5rh!'
-#DEBUG=True
+
+SECRET_KEY='phzoz(zq=!&pca(m3x8qwt*(9xs@7qkv^h8d0*-y$buo3%5rh!'
+DEBUG=True
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =os.environ.get('DEBUG')
+
 
 ALLOWED_HOSTS = [ 'zuridjangoapp.herokuapp.com']
 
